a640e5e848e038a0483eabc2275d4d45d64d96cb
unknown <utilisateur@ws20-ens-doc01.unc.prod>
2021-12-10 16:52:18 +1100

    Ajout code LSTM pour différents jeu de données et modification

---
+++ LSTM2var.py
@@ -0,0 +1,145 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 10:34:53 2021
+
+@author: Bryan Albani Van-Fouques L3 Trec7 UNC
+"""
+#Modele LSTM prenant deux jeux de données de meme taille (qte pluie et qte lepto) 
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+
+# convert an array of values into a dataset matrix
+def create_datasetX(dataset, look_back=1):
+    dataX = []
+    for i in range(look_back,len(dataset)):
+         a = dataset[i-look_back:(i), 0]
+         dataX.append(a)
+    return np.array(dataX)
+
+def create_datasetY(dataset, look_back=1):
+	dataY = []
+	for i in range(look_back,len(dataset)):
+		dataY.append(dataset[i - look_back, 0])
+	return np.array(dataY)
+
+
+# fix random seed for reproducibility and number of epochs
+np.random.seed(7)
+epochs=1000
+
+# load the dataset
+dataframe = read_csv("test2.csv", usecols=[2], engine='python')
+dataframe2 = read_csv("test2.csv", usecols=[3], engine='python')
+
+
+df1 = dataframe.values
+df1 = df1.astype('float32')
+
+df2 = dataframe2.values
+df2 = df2.astype('float32')
+
+
+# normalize the dataset
+scaler = MinMaxScaler()
+df1 = scaler.fit_transform(df1)
+
+
+df2 = scaler.fit_transform(df2)
+
+# split into train and test sets
+train_size = int(len(df1) * 0.50)
+test_size =  int(len(df1) * 0.30)
+validation_size = int( len(df1) * 0.20)
+
+train,validation,test  = df1[0:train_size],df1[train_size:train_size+validation_size] ,df1[train_size+validation_size:len(df1)]
+
+train_output = int(len(df2) * 0.50)
+test_output = int(len(df2) * 0.30)
+validation_output = int( len(df2) * 0.20)
+
+trainOutput,validationOutput ,testOutput = df2[0:train_output,:],df2[train_output:train_output+validation_output,:] ,df2[train_output+validation_output:len(df2),:]
+
+# reshape into X=t and Y=t+1
+look_back = 3
+trainX = create_datasetX(train, look_back)
+validationX = create_datasetX(validation, look_back)
+testX = create_datasetX(test, look_back)
+
+
+trainY=create_datasetY(trainOutput, look_back)
+validationY = create_datasetY(validationOutput,look_back)
+testY=create_datasetY(testOutput, look_back)
+
+# reshape input to be [samples, time steps, features]
+
+
+trainX = trainX.reshape((trainX.shape[0], trainX.shape[1], 1))
+testX = testX.reshape((testX.shape[0], testX.shape[1], 1))
+validationX = validationX.reshape((validationX.shape[0],validationX.shape[1],1))
+
+# create and fit the LSTM network
+batch_size = 1 
+model = Sequential()
+model.add(LSTM(4, batch_input_shape=(batch_size, look_back,1), stateful=True, return_sequences=True))
+model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+#for i in range(20):
+model.fit(trainX, trainY, validation_data=(validationX,validationY), epochs=epochs, batch_size=batch_size, verbose=2, shuffle=False)
+model.reset_states()
+ 
+    
+# make predictions
+trainPredict = model.predict(trainX, batch_size=batch_size)
+validationPredict = model.predict(validationX,  batch_size = batch_size)
+model.reset_states()
+testPredict = model.predict(testX, batch_size=batch_size)
+
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+#ValidScore = math.sqrt(mean_squared_error(validationY[0], trainPredict[:,0]))
+#print('Train Score: %.2f RMSE' % (ValidScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+# shift train predictions for plotting
+trainPredictPlot = np.empty_like(df1)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+
+# shift validation predictions for plotting
+validationPredictPlot = np.empty_like(df1)
+validationPredictPlot[:, :] = np.nan
+validationPredictPlot[(len(trainPredict))+(look_back*2):(len(trainPredict)+len(validationPredict)+(look_back*2)), :] = validationPredict
+
+
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(df1)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[(len(trainPredict)+len(validationPredict))+(look_back*3):len(df1), :] = testPredict
+
+
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(df2))
+plt.plot(trainPredictPlot)
+plt.plot(validationPredictPlot)
+plt.plot(testPredictPlot)
+plt.title("Epochs:%s et look_back:%s"%(epochs,look_back))
+plt.show()
--- LstmXY-2Set.py
+++ LstmXY-2Set.py
@@ -4,6 +4,7 @@ Created on Wed Nov 17 10:34:53 2021
 
 @author: Bryan Albani Van-Fouques L3 Trec7 UNC
 """
+#Modele LSTM prenant en X les données de pluie (une liste de données) et en Y la qte de leptospirose (une valeur). Il y a une jeu de données pour l'apprentissage et un autre pour le test
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas import read_csv
@@ -14,54 +15,83 @@ from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
-"""
-from saveAndLoad import save
-from saveAndLoad import load
-"""
+
 
 # convert an array of values into a dataset matrix
-def create_dataset(dataset, look_back=1):
-	dataX, dataY = [], []
+def create_datasetX(dataset, look_back=1):
+    dataX = []
 
-	for i in range(look_back,len(dataset)):
-		a = dataset[i-look_back:(i), 0]
-		dataX.append(a)
-		dataY.append(dataset[i - look_back, 0])
+    for i in range(len(dataset)-look_back-1):
+        
+        a=dataset[i:(i+look_back), 0] 
+        dataX.append(a)
+   
+       
+    return np.array(dataX)
 
-	return np.array(dataX), np.array(dataY)
+def create_datasetY(dataset, look_back=1):
+	dataY = []
+	for i in range(len(dataset)-look_back-1):
+	
+
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataY)
+
 
 # fix random seed for reproducibility and number of epochs
 np.random.seed(7)
-epochs=10
+epochs=1000
 
 # load the dataset
-dataframe = read_csv("leptoLucas.txt", usecols=[2],sep='\t', engine='python')
-dataset = dataframe.values
-dataset = dataset.astype('float32')
+dataframeX = read_csv("heurelepto2.csv", usecols=[0,1,2], engine='python')
+
+dataframeY = read_csv("heurelepto2.csv", usecols=[3], engine='python')
+
+
+df1 = dataframeX.values
+df1 = df1.astype('float32')
+
+
+
+dfY = dataframeY.values
+dfY = dfY.astype('float32')
+
 
 # normalize the dataset
 scaler = MinMaxScaler()
-dataset = scaler.fit_transform(dataset)
+
+df1 = scaler.fit_transform(df1)
+
+dfY = scaler.fit_transform(dfY)
 
 # split into train and test sets
-train_size = int(len(dataset) * 0.67)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+train_size = int(len(df1) * 0.67)
+test_size = len(df1) - train_size
+train, test = df1[0:train_size], df1[train_size:len(df1)]
+
+
+train_output = int(len(dfY) * 0.67)
+test_output = len(dfY) - train_output
+trainOutput, testOutput = dfY[0:train_output,:], dfY[train_output:len(dfY),:]
 
 # reshape into X=t and Y=t+1
 look_back = 3
-trainX, trainY = create_dataset(train, look_back)
-testX, testY = create_dataset(test, look_back)
+trainX = create_datasetX(train, look_back)
+testX = create_datasetX(test, look_back)
+
+trainY=create_datasetY(trainOutput, look_back)
+testY=create_datasetY(testOutput, look_back)
 
 # reshape input to be [samples, time steps, features]
-trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
-testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+
+trainX = trainX.reshape((trainX.shape[0], trainX.shape[1], 1))
+testX = testX.reshape((testX.shape[0], testX.shape[1], 1))
 
 # create and fit the LSTM network
-batch_size = 1 
+batch_size = 1
 model = Sequential()
-model.add(LSTM(4, batch_input_shape=(batch_size, look_back,1), stateful=True, return_sequences=True))
-model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True))
+model.add(LSTM(10, batch_input_shape=(batch_size, look_back,1), stateful=True, return_sequences=True))
+model.add(LSTM(10, batch_input_shape=(batch_size, look_back, 1), stateful=True))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 #for i in range(20):
@@ -87,24 +117,19 @@ testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
 
 # shift train predictions for plotting
-trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot = np.empty_like(df1)
 trainPredictPlot[:, :] = np.nan
 trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
 
 # shift test predictions for plotting
-testPredictPlot = np.empty_like(dataset)
+testPredictPlot = np.empty_like(df1)
 testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict)+(look_back*2):len(dataset), :] = testPredict
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(df1)-1, :] = testPredict
 
 
 # plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
+plt.plot(scaler.inverse_transform(dfY))
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
 plt.title("Epochs:%s et look_back:%s"%(epochs,look_back))
 plt.show()
-
-"""
-save(model, "C:/Users/utilisateur")
-load()
-"""
